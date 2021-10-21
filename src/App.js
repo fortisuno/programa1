910ad1f0b50c68d4b704d482eb9e56a3f436bad9
Pablo Gomez <pgomezm1400@alumno.ipn.mx>
2021-10-21 14:37:14 -0500

    Subida de archivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { Container, Typography } from '@mui/material';
 import './App.css';
+import HorizontalStepper from './components/HorizontalStepper';
+import { View } from './components/View';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const integrantes = [
+		'Hernández Ayala Oscar Uriel',
+		'Ibarra Ibarra Alexis',
+		'Gomez Garcia Jovani',
+		'Gómez Morales Pablo Arturo',
+	]
+
+	return (
+		<Container sx={{ p: '5rem'}}>
+			<Typography variant="h2" component="div" align="center">
+				Programa 1
+			</Typography>
+			<Typography variant="h3" component="div" align="center" sx={{mb: '4rem', color: 'primary.main'}}>
+				Base64
+			</Typography>
+			<HorizontalStepper>
+				<View/>
+			</HorizontalStepper>
+			<Typography variant="h6" component="div"  align="right" gutterBottom sx={{mt: '5rem'}}>
+				Equipo 4
+			</Typography>
+			{integrantes.map(integrante => (
+			<Typography variant="body1" component="div" align="right" sx={{color: 'text.secondary'}}>
+				{integrante}
+			</Typography>
+			))}
+		</Container>
+	);
 }
 
 export default App;
---
+++ src/components/View.js
@@ -0,0 +1,114 @@
+import React, { useCallback, useContext, useState, useEffect } from 'react'
+import { StepperContext } from './HorizontalStepper'
+import Box from '@mui/material/Box'
+import { Typography, TextField } from '@mui/material'
+
+const TextoPlano = ({value, handleChange, handleReset}) => {
+
+	useEffect(() => {
+		handleReset()
+	}, [])
+
+	return (
+		<React.Fragment>
+			<Typography variant="h6" sx={{mb: 3}}>
+				Introduce un texto plano
+			</Typography>
+        	<TextField
+				id="outlined-multiline-flexible"
+				label="Texto Plano"
+				multiline
+				maxRows={4}
+				value={value}
+				onChange={handleChange}
+				fullWidth
+			/>
+		</React.Fragment>
+	)
+}
+
+const Encriptado = ({value, handleChange}) => {
+	
+	const {textPlain, encrypted} = value
+
+	useEffect(() => {
+		handleChange(textPlain)
+	}, [textPlain, handleChange])
+
+	return (
+		<React.Fragment>
+			<Typography variant="h6" sx={{mb: 3}}>
+				¡El texto plano ha sido encriptado con éxito!
+			</Typography>
+			<Box sx={{}}>
+				<Typography variant="body1" component="div" sx={{p: 3, backgroundColor:'#f5f5f5', borderRadius: '10px'}}>
+					{encrypted.length > 0 ? encrypted : 'No se ha introducido un texto plano...'}
+				</Typography>
+			</Box>
+		</React.Fragment>
+	)
+}
+
+const Desencriptado = ({value, handleChange}) => {
+	
+	const {encrypted, decrypted} = value
+
+	useEffect(() => {
+		handleChange(encrypted)
+	}, [encrypted, handleChange])
+
+	return (
+		<React.Fragment>
+			<Typography variant="h6" sx={{mb: 3}}>
+				¡Se ha recuperado el texto plano con éxito!
+			</Typography>
+			<Box sx={{}}>
+				<Typography variant="body1" component="div" sx={{p: 3, backgroundColor:'#f5f5f5', borderRadius: '10px'}}>
+					{decrypted.length > 0 ? decrypted : 'No se puede desencriptar...'}
+				</Typography>
+			</Box>
+		</React.Fragment>
+	)
+}
+
+export const View = () => {
+
+	const { activeStep } = useContext(StepperContext)
+
+	const [textPlain, setTextPlain] = useState('');
+	const [encrypted, setEncrypted] = useState('');
+	const [decrypted, setDecrypted] = useState('');
+
+	const handleTextPlain = useCallback(
+		(e) => {
+			setTextPlain(e.target.value)
+		},
+		[setTextPlain],
+	)
+
+	const handleEncrypted = useCallback(
+		(textPlain) => {
+			setEncrypted(btoa(textPlain))
+		},
+		[setEncrypted],
+	)
+
+	const handleDecrypted = useCallback(
+		(encrypted) => {
+			setDecrypted(atob(encrypted))
+		},
+		[setDecrypted],
+	)
+
+	return (
+		<Box sx={{px: 1, py: 5}}>
+			{activeStep === 0 ? (
+				<TextoPlano value={textPlain} handleChange={handleTextPlain} handleReset={() => setTextPlain('')}/>
+			) : activeStep === 1 ? (
+				<Encriptado value={{textPlain, encrypted}} handleChange={handleEncrypted}/>
+				) : (
+				<Desencriptado value={{encrypted, decrypted}} handleChange={handleDecrypted}/>
+			)}
+		</Box>
+	)
+}
